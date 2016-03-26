a53eeaf828355a1437bac6d241c3c3638671b287
Nicolás Rudolph <nicolas.rudolph2@gmail.com>
2016-03-25 21:31:47 -0300

    Puertas, luces y experiencias agregadas

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class Door : MonoBehaviour {
-
+    private GameObject axis;
 	// Use this for initialization
 	void Start () {
-	
+        axis = GameObject.Find("Eje");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update() {
+        Vector3 axisPosition = axis.transform.position;
+        this.transform.RotateAround(axisPosition, Vector3.up, 20 * Time.deltaTime);
+    }
 }
