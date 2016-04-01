9967e56598c087388577ecf852d68435319ffc07
Nicolás Rudolph <nicolas.rudolph2@gmail.com>
2016-04-01 20:38:39 -0300

    commit

---
+++ Assets/Scripts/PlayerColliderController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColliderController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.gameObject.name.Equals ("CuboColor")) {
+			hit.gameObject.GetComponent<Cube> ().ChangeColor ();
+		}
+	}
+}
